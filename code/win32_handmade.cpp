3c8d0c8dd9b9205ae49e623d668bb44d5ba3f5fd
Zachary Inglis <zacharycinglis@gmail.com>
2023-11-17 06:09:34 -0600

    using the buffer to actually draw pixels

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,27 +1,33 @@
 #include <windows.h>
+#include <stdint.h>
+
 #define internal_function static
 #define local_persist static
 #define global_variable static
 
+typedef uint8_t uint8;
+typedef uint32_t uint32;
+
 global_variable bool running;
 global_variable BITMAPINFO bitmapInfo;
 global_variable void *bitmapMemory;
-global_variable HBITMAP bitmapHandle;
-global_variable HDC bitmapDeviceContext;
+global_variable int bitmapWidth;
+global_variable int bitmapHeight;
+
 
 internal_function void
 win32ResizeDIBSection(int width, int height){
 
-  if(bitmapHandle){
-    DeleteObject(bitmapHandle);
-  }
-  if(!bitmapDeviceContext){
-    bitmapDeviceContext = CreateCompatibleDC(0);
+  if(bitmapMemory){
+    VirtualFree(bitmapMemory, 0, MEM_RELEASE);
   }
 
+  bitmapWidth = width;
+  bitmapHeight = height;
+  
   bitmapInfo.bmiHeader.biSize = sizeof(bitmapInfo.bmiHeader);
-  bitmapInfo.bmiHeader.biWidth = width;
-  bitmapInfo.bmiHeader.biHeight = height;
+  bitmapInfo.bmiHeader.biWidth = bitmapWidth;
+  bitmapInfo.bmiHeader.biHeight = -bitmapHeight;
   bitmapInfo.bmiHeader.biPlanes = 1;
   bitmapInfo.bmiHeader.biBitCount = 32;
   bitmapInfo.bmiHeader.biCompression = BI_RGB;
@@ -30,21 +36,49 @@ win32ResizeDIBSection(int width, int height){
   bitmapInfo.bmiHeader.biYPelsPerMeter = 0;
   bitmapInfo.bmiHeader.biClrUsed = 0;
   bitmapInfo.bmiHeader.biClrImportant = 0;
-  
-  bitmapHandle = CreateDIBSection(bitmapDeviceContext,
-				  &bitmapInfo,
-				  DIB_RGB_COLORS,
-				  &bitmapMemory,
-				  0,
-				  0);
-  
+
+  int bytesPerPixel =4;
+  int bitmapMemorySize = (bitmapWidth*bitmapHeight)*bytesPerPixel;
+  bitmapMemory = VirtualAlloc(0, bitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+
+  int pitch = width*bytesPerPixel;
+  uint8 *row = (uint8*)bitmapMemory;
+  for(int y = 0; y < bitmapHeight; ++y){
+    uint8 *pixel = (uint8*)row;
+    for(int x = 0; x < bitmapWidth; ++x){
+      /*
+	Pixel in memory, hex: BB GG RR xx
+      */
+      *pixel = (uint8)x;
+      ++pixel;
+      
+      *pixel = (uint8)y;
+      ++pixel;
+
+      *pixel = 0;
+      ++pixel;
+
+      *pixel = 0;
+      ++pixel;
+    }
+
+    row+= pitch;
+  }
 }
 
 internal_function void
-win32UpdateWindow(HDC deviceContext, int x, int y, int width, int height){
+win32UpdateWindow(HDC deviceContext, RECT *windowRect, int x, int y, int width, int height){
+
+  int windowWidth = windowRect->right - windowRect->left;
+  int windowHeight = windowRect->bottom - windowRect->top;
+  //maybe use bitblt for better preformance
   StretchDIBits(deviceContext,
+		/*
 		x, y, width, height,
 		x, y, width, height,
+		*/
+		0, 0, bitmapWidth, bitmapHeight,
+		0, 0, windowWidth, windowHeight, 
 		bitmapMemory,
 		&bitmapInfo,
 		DIB_RGB_COLORS,// note here for palatized color
@@ -84,7 +118,10 @@ mainWindowCallback(HWND hwnd,
     int y = paint.rcPaint.top;
     int width = paint.rcPaint.right - paint.rcPaint.left;
     int height = paint.rcPaint.bottom - paint.rcPaint.top;
-    win32UpdateWindow(deviceContext, x, y, width, height);
+    RECT clientRect;
+    GetClientRect(hwnd, &clientRect);
+    
+    win32UpdateWindow(deviceContext, &clientRect, x, y, width, height);
   } break;
   default:
     result = DefWindowProc(hwnd, uMsg, wParam, lParam);
